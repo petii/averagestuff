55689d5340cf2e7320ccbb38bdc5ca9627c3844c
Vida Péter <petike997@gmail.com>
2017-02-07 18:11:25 +0100

    Added "korrigált kreditindex"
    Also the program now prints "nincs szerzett érdemjegy" if there is no
    mark instead of "crashing".

--- atlag.py
+++ atlag.py
@@ -9,7 +9,8 @@ marks = {
 	u'Jó'	: 4,
 	u'Közepes'	: 3,
 	u'Elégséges'	: 2,
-	u'Elégtelen'	: 1 
+	u'Elégtelen'	: 1, 
+	u'Elhagyott' : 0
 }
 
 def replacecomma(text):
@@ -33,15 +34,28 @@ def getVal(text):
 		line = line.split('#')
 		if len(line) > 7:
 			mark = line[7].split(' ')[0]
-			pairs.append( (int(line[2]),marks[mark]) )
-		
+			if mark == '' :
+				print line[1].split(',')[0][1:] , u'- nincs szerzett érdemjegy'
+				pairs.append( (int(line[2]),marks[u'Elhagyott']))
+			else:
+				pairs.append( (int(line[2]),marks[mark]) )
 	return pairs
 
 def calculateKI( kj ):
-	szumma = 0;
+	szumma = 0
 	for subject in kj:
 		szumma += subject[0]*subject[1]
 	return ( float(szumma) / 30 )
+
+def calculateKKI( kj ):
+	felvettkredit = 0
+	teljeskredit = 0
+	for subject in kj:
+		felvettkredit += subject[0]
+		if subject[1] > 0 :
+			teljeskredit += subject[0]
+	return calculateKI(kj) * ( float(teljeskredit) / float(felvettkredit) )
+
 	
 def process( path ):
 	file = codecs.open(path , "r" , "utf-8")
@@ -52,7 +66,8 @@ def process( path ):
 	#print content
 	kreditJegy = getVal(content)
 	#print kreditJegy
-	print 'Kreditindex:', calculateKI ( kreditJegy )
+	print u'Kreditindex:', calculateKI ( kreditJegy )
+	print u'Korrigált kreditindex:', calculateKKI ( kreditJegy )
 	
 #print marks
 
